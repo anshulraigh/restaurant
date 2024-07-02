6f1418b572fc38b612445eee874b8f2bdf7f7fe2
Anshul Rai <anshulrai02092003@gmail.com>
2024-07-02 21:52:09 +0530

    fixed adderess

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const menuItem = require('../models/MenuItem');
+const menuItem = require('../models/menuItem');
 
 router.post('/', async (req, res) => {
     try {
